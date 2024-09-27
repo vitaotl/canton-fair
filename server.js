d8ddc516f82a247ac438fe08e1110398c9227784
Victor Campos <victor.campos@grancursosonline.com.br>
2024-09-27 07:20:00 -0300

    modify serverjs

--- server.js
+++ server.js
@@ -8,27 +8,21 @@ const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
-app.prepare().then(() => {
-  if (dev) {
-    // Desenvolvimento: Use HTTP
-    createServer((req, res) => {
-      handle(req, res);
-    }).listen(21011, err => {
-      if (err) throw err;
-      console.log('> Ready on http://localhost:21011');
-    });
-  } else {
-    // Produção: Use HTTPS
-    const sslOptions = {
-      key: fs.readFileSync(path.join(__dirname, 'key/key.pem')),
-      cert: fs.readFileSync(path.join(__dirname, 'key/cert.pem'))
-    };
+console.log(path.join(__dirname, 'key/key.pem'))
 
-    https.createServer(sslOptions, (req, res) => {
-      handle(req, res);
-    }).listen(21011, err => {
-      if (err) throw err;
-      console.log('> Ready on https://localhost:21011');
-    });
-  }
+
+app.prepare().then(() => {
+  // Produção: Use HTTPS
+  const sslOptions = {
+    key: fs.readFileSync(path.join(__dirname, 'key/key.pem')),
+    cert: fs.readFileSync(path.join(__dirname, 'key/cert.pem'))
+  };
+
+  https.createServer(sslOptions, (req, res) => {
+    handle(req, res);
+  }).listen(21011, err => {
+    if (err) throw err;
+    console.log('> Ready on https://localhost:21011');
+  });
+
 });
